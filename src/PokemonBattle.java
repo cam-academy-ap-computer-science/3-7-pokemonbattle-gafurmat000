2e960962e756075c76ce4051e52cdb0355895a28
gafurmat000 <gafurmat000@CAM-LAB-17.bgsd.k12.wa.us>
2019-11-01 07:49:32 -0700

    Psudocode.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,4 +1,16 @@
-
+/* BattleStart *
+ * write a method that is called battleStart()
+ * method battleStart() prompts user to choose their first pokemon battle
+ * battleStart returns the name of the pokemon chosen
+ * battleStart will be one of the methods in main
+ * 
+ * Damage *
+ * Next method is gonna be called damage()
+ * damage takes a pokemon's name and uses it as a parameter and returns hp after damage is done
+ * using the equation to find damage. damage = (2*Level+10/250 + Attack/Defense * Base + 2) * Modifier
+ * find the modifier by using this equation, modifier = Same Type attack bonus (STAB) * Random
+ * statsTable
+ */
 public class PokemonBattle {
 
 	public static void main(String[] args) {
