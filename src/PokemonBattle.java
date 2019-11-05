80a83dc9a1094c3b3d80bf25e991a93aab3e09b5
gafurmat000 <gafurmat000@CAM-LAB-17.bgsd.k12.wa.us>
2019-11-05 07:33:14 -0800

    fisrt part complete.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -9,13 +9,25 @@
  * damage takes a pokemon's name and uses it as a parameter and returns hp after damage is done
  * using the equation to find damage. damage = (2*Level+10/250 + Attack/Defense * Base + 2) * Modifier
  * find the modifier by using this equation, modifier = Same Type attack bonus (STAB) * Random
- * statsTable
+ * 
+ * statsTable *
+ * create a method called statsTable
+ * the table should show up, that accepts the user’s Pokemon name, and stats as parameters
  */
+import java.util.*;  // Scanner
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner console = new Scanner(System.in);
+		battleStart();
+		/* Damage();
+		StatsTable(); */
 	}
-
+	public static void battleStart() {
+		/* Scanner userInput = new Scanner (System.in) { 
+			System.out.println("Welcome to Pokemon battle, when ready, choose your fighter.");
+		String username = userInput.nextLine();
+			System.out.println(username + " Is a great choice."); */
+		}
 }
+
