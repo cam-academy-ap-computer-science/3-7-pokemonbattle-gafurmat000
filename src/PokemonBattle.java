f665ab6815e245dc2d7848081170658af0489009
GAFUR <GAFUR@192.168.0.29>
2019-11-06 22:13:39 -0800

    Complete.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -17,17 +17,58 @@
 import java.util.*;  // Scanner
 public class PokemonBattle {
 
+	static Scanner userInput = new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		battleStart();
-		/* Damage();
-		StatsTable(); */
+		String username = battleStart();
+		int hp = Damage(username);
+		StatsTable(username, hp); 
 	}
-	public static void battleStart() {
-		/* Scanner userInput = new Scanner (System.in) { 
-			System.out.println("Welcome to Pokemon battle, when ready, choose your fighter.");
-		String username = userInput.nextLine();
-			System.out.println(username + " Is a great choice."); */
-		}
+	public static String battleStart() {
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println("Zebstrika appeared.");
+		System.out.println("Which Pokemon do you choose?");
+			String username = userInput.nextLine();
+			System.out.println("You chose " + username + "!");
+			System.out.println("It's a Pokemon battle between Zebstrika and " + username + "! Go!");
+			System.out.println();
+			return username;
+	}
+	public static int Damage(String username) {
+		System.out.println("Zebstrika used Thunderbolt!");
+		System.out.println("Trainer what are your " + username + "'s stats?");
+		System.out.print("Level: ");    
+		int Level = userInput.nextInt();
+		System.out.print("Attack: ");
+		int Attack = userInput.nextInt();
+		System.out.print("Defense: ");
+		int Defense = userInput.nextInt();
+		System.out.print("Base: ");
+		int Base = userInput.nextInt();
+		System.out.print("STAB: ");
+		int STAB = userInput.nextInt();
+		System.out.print("HP: ");
+		int HP = userInput.nextInt();
+		System.out.println();
+		double Modifier = (0.85 + (Math.random() * 0.15)) * STAB; //equation for modifier
+		int Damage = (int)((((2 * Level + 10) / (250)) + ((Attack / Defense) * Base) + 2) * Modifier); //
+		System.out.println(username + " sustained " + Damage + " points of damage."); 
+		int hp = HP - 10; // new hp after attack
+		System.out.println("HP, after damage, is now " + hp);
+		System.out.println();
+		return hp;
+		
+	}
+	public static void StatsTable(String username,int hp) { 
+		System.out.println("Name     "+ username);
+		System.out.println("Level     40");
+		System.out.println("------------------------------"); 
+		System.out.println("HP     " + hp);
+		System.out.println("Attack     51");
+		System.out.println("Defense    51");
+		System.out.println("Sp. Atk   121");
+		System.out.println("Sp. Def   81");
+		System.out.println("Speed 	  107");
+		System.out.println("------------------------------"); 
+	}
 }
-
+		
